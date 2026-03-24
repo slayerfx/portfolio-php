d58f79196670b90138937c9ed1b85791256fcd3a
slayerfx <louenn.p@gmail.com>
2026-03-24 12:29:52 +0100

    Fix: mise à jour des langages et descriptions des projets

--- sql/insert_projects.sql
+++ sql/insert_projects.sql
@@ -1,7 +1,7 @@
 INSERT INTO project (name, description, language, source_link, github_link) VALUES
-('Robbie Lens', 'Site vitrine pour un photographe', 'HTML', '', 'https://github.com/slayerfx/robbie-lens'),
-('HiTech', 'Site vitrine pour une entreprise tech', 'HTML', '', 'https://github.com/slayerfx/hitech'),
-('Freshly Restaurant', 'Site pour un restaurant avec fonctionnalités JavaScript', 'JavaScript', '', 'https://github.com/slayerfx/freshly-restaurant'),
-('Archi Design', 'Site vitrine pour un cabinet d''architecture', 'HTML', '', 'https://github.com/slayerfx/archi-design'),
-('To-Do List', 'Application de gestion de tâches', 'CSS', '', 'https://github.com/slayerfx/to-do-list'),
-('Portfolio', 'Site portfolio personnel', 'HTML', '', 'https://github.com/slayerfx/portfolio');
+('Robbie Lens', 'Site vitrine responsive pour un photographe', 'HTML/CSS', '', 'https://github.com/slayerfx/robbie-lens'),
+('HiTech', 'Vitrine tech avec intégration d''un carrousel d''images', 'HTML/CSS/JavaScript', '', 'https://github.com/slayerfx/hitech'),
+('Freshly Restaurant', 'Site pour un restaurant avec fonctionnalités dynamiques', 'JavaScript', '', 'https://github.com/slayerfx/freshly-restaurant'),
+('Archi Design', 'Site vitrine pour un cabinet d''architecture', 'HTML/CSS', '', 'https://github.com/slayerfx/archi-design'),
+('To-Do List', 'Application de gestion de tâches interactive', 'JavaScript/CSS/HTML', '', 'https://github.com/slayerfx/to-do-list'),
+('Portfolio', 'Site portfolio personnel responsive avec section CV', 'HTML/CSS/JavaScript', '', 'https://github.com/slayerfx/portfolio');
